4697afc3d59459e62bd1dce5e4cd43ba42e06fc0
Prab Hiranayachatri <sys10m@Prabs-MacBook-Air-2.local>
2024-01-26 02:08:18 +0000

    add readme

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Ticketing / emailing system for TCD Asian ball 23/24
 
 - The website for the buyers will produce QR code and reference message to put in revolut transfer and then add the information to firebase database
+
+- The website for the treasurer let the treasurer access key in paid bookings => it will send an email with the ticket
+
+    - should the treasurer see all the bookings and tick
+
+- need ticket generator that adds the barcode to the ticket + details
+
+- post sell app for checking in tickets
--- buying-site/index.js
+++ buying-site/index.js
@@ -1,7 +1,10 @@
+import { barcodeHash } from "./barcodeHash.js";
+
 const nameInput = document.querySelector("#name-input")
 const idInput = document.querySelector("#id-input")
 const submitBtn = document.querySelector("#submit-btn")
 
+console.log("run")
 // connect to firebase
 
 submitBtn.addEventListener("click", function(){
@@ -9,10 +12,14 @@ submitBtn.addEventListener("click", function(){
     let booking = {
         name: nameInput.value,
         id: idInput.value,
-        barcodeHash: "test",
+        barcodeHash: barcodeHash(),
         paid: false,
         revMessage: `${nameInput.value}`
     }
 
     console.log(booking)
+    // add to firebase
+
+    // pop up qr and payment details
+
 })
