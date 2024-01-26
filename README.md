8eac54ee638750c3175b6e577201c90897a414f9
Prab Hiranayachatri <sys10m@Prabs-MacBook-Air-2.local>
2024-01-26 01:43:28 +0000

    skeleton

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tcd-asianball-ticket-2324
 Ticketing / emailing system for TCD Asian ball 23/24
+
+- The website for the buyers will produce QR code and reference message to put in revolut transfer and then add the information to firebase database
---
+++ buying-site/index.js
@@ -0,0 +1,16 @@
+const nameInput = document.querySelector("#name-input")
+const idInput = document.querySelector("#id-input")
+const submitBtn = document.querySelector("#submit-btn")
+
+// connect to firebase
+
+submitBtn.addEventListener("click", function(){
+    // hash function
+    let booking = {
+        name: nameInput.value,
+        id: idInput.value,
+        barcodeHash: "test",
+        paid: false,
+        revMessage: `${nameInput.value}`
+    }
+})
